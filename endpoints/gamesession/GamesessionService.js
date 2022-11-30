2a356bc10aea0aebaa7d874d3b58217d163b7079
Anser <aj@wemove.com>
2022-11-30 21:26:53 +0100

    add createGamesession

--- endpoints/gamesession/GamesessionService.js
+++ endpoints/gamesession/GamesessionService.js
@@ -1,5 +1,37 @@
 const Gamesession = require("./GamesessionModel")
+const Lobby = require("../lobby/LobbyModel")
 
-//TODO
+function getGamesessions() {
+  Gamesession.find((error, gamesessions) => {
+    if(error) return callback (err, null)
+    if(gamesessions) return callback(null, gamesessions)
+    return callback(new Error("Something went wrong"), null)
+  })
+}
 
-module.exports = {}
+function createGamesession(gamesessionData) {
+  Lobby.findOne({_id: gamesessionData.lobby}, (error, lobby) => {
+    if(error) return callback(error, null)
+    Gamesession.create({
+      gamemode: lobby.gamemode,
+      users: lobby.users
+    }, (error, gamesession) => {
+      if(error) return callback(error, null)
+      if(gamesession) {
+        return callback(
+          null,
+          {
+            _id: gamesession._id,  
+            gamemode: gamesession.gamemode,
+            users: gamesession.users
+          }
+        )
+      }
+    })
+  })
+}
+
+module.exports = { 
+  getGamesessions,
+  createGamesession
+}
